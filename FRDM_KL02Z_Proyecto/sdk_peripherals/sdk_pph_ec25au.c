8031e2f4c35d777dddc06564748983cac7772f36
luiscastrillo97 <luiscastrillojf@gmail.com>
2021-01-31 12:33:07 -0500

    Cambiando tiempo máximo de espera.

--- FRDM_KL02Z_Proyecto/sdk_peripherals/sdk_pph_ec25au.c
+++ FRDM_KL02Z_Proyecto/sdk_peripherals/sdk_pph_ec25au.c
@@ -29,7 +29,7 @@ enum _ec25_lista_ordendes{
 };
 
 #define EC25_BYTES_EN_BUFFER	100
-#define EC25_TIEMPO_MAXIMO_ESPERA	10
+#define EC25_TIEMPO_MAXIMO_ESPERA	20
 /*******************************************************************************
  * Private Prototypes
  ******************************************************************************/
